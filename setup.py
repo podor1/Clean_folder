aa231cbbfcc1127d36c025b458feae61fa1a7b9f
Andrii Podorvan <138048362+podor1@users.noreply.github.com>
2023-08-30 22:41:56 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='clean_folder',
-      version='0.0.1',
-      packages=find_packages(),
-      author='GO_IT_Studen',
-      description='Useful package to sort and manage file in required directory',
-      entry_points={'console_scripts':['clean-folder = clean_folder.clean:main']})
